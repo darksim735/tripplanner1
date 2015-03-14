01d8c00fdcf7c91085d85b402528e13ac2c1199d
darksim735 <darksim735@gmail.com>
2015-03-14 12:01:57 -0400

    added express.static paths, trying to access jquery and other dependencies

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var async = require('async');
 var swig = require('swig');
 
 var routes = require('./routes/routes');
-
+var path = require('path');
 var app = express();
 
 app.use('/', routes);
+app.use(express.static(path.join(__dirname, '/bower_components')));
+//app.use(express.static(__dirname + '/bower_components'));
+
+swig.setDefaults({cache: false});
 app.engine('html', swig.renderFile);
+
 app.set('view engine', 'html');
 
 app.listen(3000);
