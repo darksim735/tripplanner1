f0de55a73300acdc2f200395509ae4d0c0da9e26
Alex Liu <yuningalexliu@gmail.com>
2015-03-14 11:35:14 -0400

    set up basic route for index and form dropdowns

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-
-
 var express = require('express');
+var async = require('async');
+var swig = require('swig');
+
+var routes = require('./routes/routes');
 
 var app = express();
+
+app.use('/', routes);
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
 app.listen(3000);
+
+module.exports = app;
---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models/index');
+
+/* GET HOME PAGE */
+router.get('/', function(req, res, next) {
+	res.render('index');
+});
+
+module.exports = router;
